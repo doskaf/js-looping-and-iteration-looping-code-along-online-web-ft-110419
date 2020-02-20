708bba2a0ae6bb94a67d9c189c4782348d098d80
Dominique Skaf <doskaf@yahoo.com>
2020-02-20 22:43:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function writeCards(names, eventName) {
   for (let i = 0; i < names.length; i++) {
     let thankYouMsgs = [];
-    thankYouMsgs.push(`Thank you, ${names[i]}, for the wonderful birthday gift!`);
+    thankYouMsgs.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
   }
   return thankYouMsgs;
 }
